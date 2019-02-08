71f15ee3467f7e26b3f85881675555cbfe8e430b
lakith <lakith1995@gmail.com>
2019-02-09 00:22:09 +0530

    basic hooks with array destructuring

---
+++ src/components/Todo.js
@@ -0,0 +1,29 @@
+import React,{useState} from 'react'
+
+const todo = props => {
+
+    const [todoName,setTodoName] = useState('')
+
+    const inputChangeHandler = (event) => {
+        setTodoName(event.target.value)
+    }
+
+
+    return(
+        <React.Fragment>
+            <input 
+                type="text"
+                placeholder="Todo"
+                onChange={inputChangeHandler}
+                value={todoName}
+            />
+            <button type="button">Add</button>
+            <ul>
+                <li>list</li>
+            </ul>
+        </React.Fragment>
+    )
+}
+
+export default todo
+
